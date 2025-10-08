f94ddc5ca7270de3861d3ac544155ae4fcce6e27
Vini <zmisterfire@gmail.com>
2025-10-08 15:09:24 -0300

    Nova base do site JS puro8

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,21 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <h1>💍 Bingo Fotográfico</h1>
-      <p>Envie suas fotos e compartilhe o momento!</p>
+      <div className="header-container">
+        <img
+          src="/bingo-fotografico.png"
+          alt="Logotipo Bingo Fotográfico"
+          className="logo"
+        />
+        <div>
+          <h1>💍 Bingo Fotográfico</h1>
+          <p>Envie suas fotos e compartilhe o momento!</p>
+        </div>
+      </div>
       <nav>
         <Link href="/">Galeria</Link>
         <Link href="/upload">Enviar Foto</Link>
       </nav>
     </header>
-  )
+  );
 }
